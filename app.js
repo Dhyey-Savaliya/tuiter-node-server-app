9c6182e1aee1847cc08ea68ec5ee98b91486663d
Dhyey-Savaliya <dhyeysavaliya2000@gmail.com>
2023-06-15 20:29:57 -0400

    a6 Mongo su23

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ import AuthController from "./users/auth-controller.js";
 import cors from 'cors'
 const app = express();
 
- 
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
+
 // app.use(cors());
 
 // app.use((req, res, next) => {
